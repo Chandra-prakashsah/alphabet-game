0f3bfd02a2e0757479c0ddd8c2e3574a43119756
Chandra Prakash Sah <98117712+Chandra-prakashsah@users.noreply.github.com>
2022-07-15 16:10:49 +0530

    assignment complete

---
+++ src/Container/Buttons.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react'
+
+const Buttons = (props) => {
+  return (
+    <>
+        <button
+            className="btn"
+            type="button"
+            onClick={() => {
+                props.isActive  ? props.reset() : props.start()
+            }}
+        >
+            {props.isActive ? 'Reset' : 'Start'}
+        </button>
+    </>
+  )
+}
+
+export default Buttons
